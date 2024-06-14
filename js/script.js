fd785370d40f7c8eb3ae5ca5404ce1ffe613bba8
gitsaurabhsingh <saurabhsurvanshi2002@gmail.com>
2024-06-14 13:38:27 -0700

    js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(function () {
+  $("a[href*=#]:not([href=#])").click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        );
         return false;
       }
     }
   });
 });
+
+function updateTimer() {
+  const future = Date.parse("june 30, 2024 11:30:00");
+  const now = new Date();
+  const diff = future - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const mins = Math.floor(diff / (1000 * 60));
+  const secs = Math.floor(diff / 1000);
+
+  const d = days;
+  const h = hours - days * 24;
+  const m = mins - hours * 60;
+  const s = secs - mins * 60;
+
+  document.getElementById("days").innerHTML = d;
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("minutes").innerHTML = m;
+  document.getElementById("seconds").innerHTML = s;
+}
+setInterval("updateTimer()", 1000);
